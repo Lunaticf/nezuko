fccf6a258cef456e8adc948b11e908ea180287c9
Lunaticf <724922441@qq.com>
2019-09-07 23:24:31 +0800

    add some other config

--- src/main/java/lunaticf/nezuko/controller/HelloController.java
+++ src/main/java/lunaticf/nezuko/controller/HelloController.java
@@ -1,14 +1,17 @@
 package lunaticf.nezuko.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class HelloController {
     @GetMapping("/")
     @ResponseBody
     public String hello() {
+        log.info("hello");
         return "hello";
     }
 }
