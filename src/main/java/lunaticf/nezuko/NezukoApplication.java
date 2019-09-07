49e1d6d360c08c20e39d610beac08413c3cc58a7
Lunaticf <724922441@qq.com>
2019-09-07 21:06:22 +0800

    add profile and druid config

--- src/main/java/lunaticf/nezuko/NezukoApplication.java
+++ src/main/java/lunaticf/nezuko/NezukoApplication.java
@@ -3,6 +3,9 @@ package lunaticf.nezuko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lunaticf
+ */
 @SpringBootApplication
 public class NezukoApplication {
 
